fe8a23edbad37686647c487ebea3401db0add195
myongwon.seo <myongwoni@gmail.com>
2023-02-26 19:29:56 +0900

    practice Section 1. 타입스크립트 소개와 배경

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,6 +9,30 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} city
+ * @property {string} street
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} username
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fatchUser() {
+  return axios.get(url);
+}
+
+fatchUser().then(function (response) {
+  response.address.city;
+})
+
 function startApp() {
   axios
     .get(url)
@@ -16,6 +40,9 @@ function startApp() {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.city;
     })
     .catch(function (error) {
       console.log(error);
---
+++ why-ts/sample.js
@@ -0,0 +1,15 @@
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function add(a, b) {
+  return a + b;
+}
+
+let result = add(10, 20);
+console.log(result);
