5c9f5e74957c6a029c7e7c07591ea719e473a224
myongwon.seo <myongwoni@gmail.com>
2023-03-01 10:52:41 +0900

    practice Section 6. 타입 별칭

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,22 @@
+// interface Gagman {
+//   name: string,
+//   age: number
+// }
+
+type Gagman = {
+  name: string,
+  age: number
+}
+
+let jaeseok: Gagman = {
+  name: '재석',
+  age: 55
+}
+
+type MyString = string;
+let str: MyString = 'hello';
+
+type Todo = { id: number, title: string, done: boolean };
+function getTodo(todo: Todo) {
+
+}
